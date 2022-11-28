96fed5bf9c66bf3bcc5d3b3ce4ba73816bddf373
NARESH CHOUDHARY <107427909+Nareshchoudhary02@users.noreply.github.com>
2022-11-28 18:35:45 +0530

    hide passwords

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-naresh:9mQAm1ma0VBgmT33@cluster0.nrpd5rt.mongodb.net/blogDB")
+mongoose.connect("mongodb+srv://<USERNAME>:<PASSWORD>@cluster0.nrpd5rt.mongodb.net/blogDB")
 
 const postSchema = new mongoose.Schema({
   title : String,
